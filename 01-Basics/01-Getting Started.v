94f8367a82e17752fd5a696fa426acc201bb3b82
mannraval1 <78301327+mannraval1@users.noreply.github.com>
2024-09-30 18:02:20 -0400

    Create 01-Getting Started.v

---
+++ 01-Basics/01-Getting Started.v
@@ -0,0 +1,8 @@
+module top_module( output one );
+// Problem Statement
+// Build a circuit with no inputs and outputs
+// The output of the circuit should always drive 1 or HIGH logic
+// Insert your code here
+    assign one = 1'b1;
+
+endmodule

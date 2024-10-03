702e26f4bd228e94492b7d3627356c260fba43f4
mannraval1 <78301327+mannraval1@users.noreply.github.com>
2024-10-03 14:44:33 -0400

    Create 02 - Output Zero.v

---
+++ 01-Basics/02 - Output Zero.v
@@ -0,0 +1,8 @@
+module top_module(
+    output zero
+);// Module body starts after semicolon
+    // Problem Statement 
+    // Build a circuit with no inputs and one ouput that is constant LOW or ZERO
+assign zero = 0;
+    
+endmodule

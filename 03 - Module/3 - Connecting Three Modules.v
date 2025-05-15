a84297603ce1c290875c96578ae84af29a7ffb6b
mannraval1 <78301327+mannraval1@users.noreply.github.com>
2025-05-15 12:30:56 -0400

    Create 3 - Connecting Three Modules.v

---
+++ 03 - Module/3 - Connecting Three Modules.v
@@ -0,0 +1,14 @@
+module top_module ( input clk, input d, output q );
+    /*
+    	You are given a module my_dff with two inputs and one output (that implements a D flip-flop).
+        Instantiate three of them, then chain them together to make a shift register of length 3.
+        The clk port needs to be connected to all instances
+        The module provided to you is: module my_dff ( input clk, input d, output q );
+    */
+    
+wire wire1, wire2;
+    my_dff dff1(.clk(clk), .d(d), .q(wire1));
+    my_dff dff2(.clk(clk), .d(wire1), .q(wire2));
+    my_dff dff3(.clk(clk), .d(wire2), .q(q));
+    
+endmodule

73ab554fbb735683977c919ccde459471f2fb29b
mannraval1 <78301327+mannraval1@users.noreply.github.com>
2025-06-12 00:04:37 -0400

    Create: 7 - PS2KeyboardController.v

---
+++ 04 - Procedures/7 - PS2KeyboardController.v
@@ -0,0 +1,29 @@
+// synthesis verilog_input_version verilog_2001
+module top_module (
+    input [15:0] scancode,
+    output reg left,
+    output reg down,
+    output reg right,
+    output reg up  ); 
+    
+    /*
+    	16'he06b	left arrow
+		16'he072	down arrow
+		16'he074	right arrow
+		16'he075	up arrow
+		Anything else	none 
+    */
+    
+    always @(*) begin 
+    	up = 1'b0; down = 1'b0; left = 1'b0; right = 1'b0;
+        case(scancode)
+            16'he06b:	begin  up = 1'b0; down = 1'b0; left = 1'b1; right = 1'b0; end
+            16'he072: begin up = 1'b0; down = 1'b1; left = 1'b0; right = 1'b0; end
+			16'he074: begin up = 1'b0; down = 1'b0; left = 1'b0; right = 1'b1; end
+            16'he075: begin up = 1'b1; down = 1'b0; left = 1'b0; right = 1'b0; end
+        endcase
+        
+    end
+    
+
+endmodule

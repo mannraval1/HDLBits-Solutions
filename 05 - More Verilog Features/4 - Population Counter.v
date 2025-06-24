ebfbf1eeb14d01a30bdc31d3fa93b17d82dcb73e
mannraval1 <78301327+mannraval1@users.noreply.github.com>
2025-06-24 12:10:13 -0400

    Create: 4 - Population Conter.v

--- 05 - More Verilog Features/4 - Population Counter.v
+++ 05 - More Verilog Features/4 - Population Counter.v
@@ -5,8 +5,6 @@ module top_module(
     /*
    		A "population count" circuit counts the number of '1's in an input vector.
         Build a population count circuit for a 255-bit input vector.
-
-
     */
     int i; 
     

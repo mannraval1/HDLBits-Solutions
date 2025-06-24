d09dd4fa3246f4dabe2d6593800cd82247510556
mannraval1 <78301327+mannraval1@users.noreply.github.com>
2025-06-24 11:58:21 -0400

    Create: 3 - Vector Reversal.v

---
+++ 05 - More Verilog Features/3 - Vector Reversal.v
@@ -0,0 +1,16 @@
+module top_module( 
+    input [99:0] in,
+    output [99:0] out
+);
+/*
+	Given a 100-bit input vector [99:0], reverse its bit ordering.
+*/
+    integer i;
+    
+    always@(*) begin
+        for(i = 0 ; i < 100; i++) begin
+            out[i] = in[99 - i];
+        end
+    end
+    
+endmodule

130c0860e7ce94e0e687e29568804978bf428f22
mannraval1 <mannraval1@gmail.com>
2026-01-23 23:53:44 -0800

    Kmaps

--- 06 - Combinational Logic/3 - KMaps/7 - KMap using MUX.v
+++ 06 - Combinational Logic/3 - KMaps/7 - KMap using MUX.v
@@ -1,11 +1,13 @@
 module top_module (
-    input a,
-    input b,
     input c,
     input d,
-    output out_sop,
-    output out_pos
+    output [3:0] mux_in
 ); 
-    assign out_sop = (c&d)|(~a&~b&c);
-    assign out_pos = c&(~b|~c|d)&(~a|~c|d);
+
+    // Mapping derived from the K-map columns:
+    assign mux_in[0] = c | d;   // Column ab=00
+    assign mux_in[1] = 1'b0;    // Column ab=01
+    assign mux_in[2] = ~d;      // Column ab=10
+    assign mux_in[3] = c & d;   // Column ab=11
+
 endmodule
